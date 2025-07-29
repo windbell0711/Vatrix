e70a2941e8415923b38d378a0c29dc1b56d4e76f
windbell0711 <windbell07@outlook.com>
2025-07-29 13:03:09 +0800

    del key

--- user_coding_place/0_0.py
+++ user_coding_place/0_0.py
@@ -1,28 +1,32 @@
 import pvz_hacker as vb
 import adventure
 
-adventure.import_data()
-adventure.levels_pool[20].show(vb.game)
+def solve():
+    adventure.import_data()
+    adventure.levels_pool[20].show(vb.game)
 
-vb.setting_up(background_running=True)
+    vb.setting_up(background_running=True)
 
-print("coding start")
-ps: list[tuple[int, int]] = [
-    (0, 3),
-    (0, 7),
-    (1, 3),
-    (1, 7),
-    (2, 3),
-    (2, 4),
-    (2, 6),
-    (2, 7),
-    (3, 4),
-    (3, 5),
-    (3, 6),
-    (4, 5)
-]
+    print("coding start")
+    ps: list[tuple[int, int]] = [
+        (0, 3),
+        (0, 7),
+        (1, 3),
+        (1, 7),
+        (2, 3),
+        (2, 4),
+        (2, 6),
+        (2, 7),
+        (3, 4),
+        (3, 5),
+        (3, 6),
+        (4, 5)
+    ]
 
-for p in ps:
-    vb.brk(x=p[1] - 1, y=p[0])
+    for p in ps:
+        vb.brk(x=p[1] - 1, y=p[0])
 
-print("coding end")
+    print("coding end")
+
+if __name__ == '__main__':
+    solve()
--- user_coding_place/1_1.py
+++ user_coding_place/1_1.py
@@ -1,20 +1,28 @@
 import pvz_hacker as vb
 import adventure
 
-for loop in range(20):
-    adventure.import_data()
-    adventure.levels_pool[0].show(vb.game)
+lvl_name = "1-1"
+lvl_num = {"1-1": 0, "1-2": 1, "1-3": 2, "1-4": 3, "1-6": 5, "1-7": 6, "1-8": 7}
 
-    vb.setting_up(background_running=True,
-                  speed_rate=5)
+def solve():
+    adventure.import_data()
+    adventure.levels_pool[lvl_num[lvl_name]].show(vb.game)
 
     print("coding start")
 
-    vb.brk(8, 2, after_delay=0.225)
-    vb.brk(7, 2, after_delay=0.225)
-
-    vb.delay(15)
-    while not vb.check_winning():
-        vb.delay(1)
+    vb.brk(8, 2, after_delay=0.23)
+    vb.brk(7, 2, after_delay=0.23)
 
     print("coding end")
+
+
+if __name__ == '__main__':
+    # vb.setting_up(background_running=True,
+    #               speed_rate=5)
+    # for loop in range(20):
+        solve()
+    #     vb.delay(10)
+    #     while not vb.check_winning():
+    #         vb.delay(2)
+    # vb.setting_up(background_running=True,
+    #               speed_rate=1)
